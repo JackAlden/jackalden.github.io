ca3aae9c49302e3fdef2eb23e73cc2739664c384
Jack <jackalden1989@gmail.com>
2019-03-18 02:05:25 +0000

    lyrics2csv v1.00

--- lyrics2csv.js
+++ lyrics2csv.js
@@ -2,6 +2,7 @@ var myStringArray = [];
 var table = "";
 
 function addWordAndSentence(){ 
+
 	//get selected word
 	var lyricsTextArea = document.getElementById('lyricsTextArea');
 	var wordSelection = lyricsTextArea.value.substring(lyricsTextArea.selectionStart, lyricsTextArea.selectionEnd);
@@ -34,11 +35,31 @@ function outputTable(){
     		table += "<td>" + myStringArray[i] + "</td></tr>";
     	}
     }
-    console.log(table)
+    //console.log(table)
+    //alert(table)
     document.getElementById('wordList').innerHTML = table;
 }
 
 function clearSelection(){
 	if (window.getSelection) {window.getSelection().removeAllRanges();}
 	else if (document.selection) {document.selection.empty();}
+}
+
+function getCSV(){
+	table = "";
+    for (var i = 0; i < myStringArray.length; i++) {
+    	if(i % 2 === 0){
+    		table += myStringArray[i] + ",";
+    	}
+    	else{
+    		table += myStringArray[i] + ",\r\n";
+    	}
+    }
+    //console.log(table)
+    //var BOM = "\uFEFF";
+    //var csv = BOM + table;
+	var myLink = document.createElement('a');
+	myLink.download = "csvname.csv";
+	myLink.href = "data:text/csv;charset=utf-8," + (table);
+	myLink.click();
 }
